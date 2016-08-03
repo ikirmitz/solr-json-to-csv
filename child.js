72a73d0ecfebaf3fc19abfec0b02038222f67f88
ikirmitz <ioanniskirmitzoglou@gmail.com>
2016-08-03 13:21:12 +0100

    First working version. TODO: Update README

--- child.js
+++ child.js
@@ -2,7 +2,9 @@
  * Created by Ioann on 20/7/2016.
  */
 
-const     request = require('request'),
+"use strict";
+
+const request = require('request'),
     jsonCsv = require('json-csv'),
     jsonStream = require('JSONStream');
 
@@ -12,45 +14,233 @@ var stream = jsonStream.parse('*.docs.*');
 //     console.log('value:', data);
 // });
 
-var options = {
-    fields: [
-        {
-            name: 'accession',
-            label: 'Accession',
-            quoted: false
-        },
-        {
-            name: 'collection_date_range_ss',
-            label: 'Collection date range',
-            quoted: true,
-            filter: function (dates) {
-                var newDates = [];
-                for (var i = 0, len = dates.length; i < len; i++) {
-                    // console.dir(dates[i])
-                    newDates.push(dates[i].replace(/[\[\]]/g, ''));
-                }
-                return newDates
+var options = {};
+var path = process.argv[3];
+// console.log(process.argv[3]);
+
+if (/irExport$/.test(path)) {
+    options = {
+        fields: [
+            {
+                name: 'exp_accession_s',
+                label: 'Accession',
+                quoted: false
+            },
+            {
+                name: 'exp_bundle_name_s',
+                label: 'Bundle Name',
+                quoted: false
+            },
+            {
+                name: 'exp_species_s',
+                label: 'Species',
+                quoted: false
+            },
+            {
+                name: 'exp_sample_type_s',
+                label: 'Sample type',
+                quoted: false
+            },
+            {
+                name: 'exp_sample_size_i',
+                label: 'Sample size',
+                quoted: false
+            },
+            {
+                name: 'exp_label_s',
+                label: 'Label',
+                quoted: true
+            },
+            {
+                name: 'exp_collection_date_range_ss',
+                label: 'Collection date range',
+                quoted: true,
+                filter: function (dates) {
+                    var newDates = [];
+                    for (var i = 0, len = dates.length; i < len; i++) {
+                        // console.dir(dates[i])
+                        newDates.push(dates[i].replace(/[\[\]]/g, ''));
+                    }
+                    return newDates
+                },
+            },
+            {
+                name: 'exp_collection_protocols_ss',
+                label: 'Collection protocols',
+                quoted: true
+            },
+            {
+                name: 'exp_projects_ss',
+                label: 'Projects',
+                quoted: true
+            },
+            {
+                name: 'exp_geo_coords_s',
+                label: 'Coordinates',
+                quoted: true
+            },
+            {
+                name: 'exp_geolocations_ss',
+                label: 'Locations',
+                quoted: true
+            },
+
+
+            {
+                name: 'exp_phenotype_type_s',
+                label: 'Phenotype type',
+                quoted: false
+            },
+            {
+                name: 'exp_insecticide_s',
+                label: 'Insecticide',
+                quoted: true
+
+            },
+            {
+                name: 'exp_protocols_ss',
+                label: 'Protocols',
+                quoted: true
+
+            },
+            {
+                name: 'exp_concentration_f',
+                label: 'Concentration',
+                quoted: false
+
+            },
+            {
+                name: 'exp_concentration_unit_s',
+                label: 'Concentration unit',
+                quoted: false
+
+            },
+            {
+                name: 'exp_duration_f',
+                label: 'Duration',
+                quoted: false
+
+            },
+            {
+                name: 'exp_duration_unit_s',
+                label: 'Duration unit',
+                quoted: false
+
+            },
+            {
+                name: 'exp_phenotype_value_f',
+                label: 'Phenotype value',
+                quoted: false
+
+            },
+            {
+                name: 'exp_phenotype_value_unit_s',
+                label: 'Phenotype value unit',
+                quoted: false
+
+            },
+            {
+                name: 'exp_phenotype_value_type_s',
+                label: 'Phenotype value type',
+                quoted: false
+
             }
-        },
-        {
-            name: 'phenotype_value_f',
-            label: 'Phenotype_value',
-            quoted: false
-        },
-        {
-            name: 'label_s',
-            label: 'Label',
-            quoted: true
 
-        }
+        ]
+    };
+} else {
+    options = {
+        fields: [
+            {
+                name: 'exp_accession_s',
+                label: 'Accession',
+                quoted: false
+            },
+            {
+                name: 'exp_bundle_name_s',
+                label: 'Bundle Name',
+                quoted: false
+            },
+            {
+                name: 'exp_species_s',
+                label: 'Species',
+                quoted: false
+            },
+            {
+                name: 'exp_sample_type_s',
+                label: 'Sample type',
+                quoted: false
+            },
+            {
+                name: 'exp_label_s',
+                label: 'Label',
+                quoted: true
+            },
+            {
+                name: 'exp_collection_date_range_ss',
+                label: 'Collection date range',
+                quoted: true,
+                filter: function (dates) {
+                    var newDates = [];
+                    for (var i = 0, len = dates.length; i < len; i++) {
+                        // console.dir(dates[i])
+                        newDates.push(dates[i].replace(/[\[\]]/g, ''));
+                    }
+                    return newDates
+                },
+            },
+            {
+                name: 'exp_collection_protocols_ss',
+                label: 'Collection protocols',
+                quoted: true
+            },
+            {
+                name: 'exp_projects_ss',
+                label: 'Projects',
+                quoted: true
+            },
+            {
+                name: 'exp_geo_coords_s',
+                label: 'Coordinates',
+                quoted: true
+            },
+            {
+                name: 'exp_geolocations_ss',
+                label: 'Locations',
+                quoted: true
+            }
 
-    ]
-};
+        ]
+    };
+}
+
+
+
 
 // console.log(process.argv[2]);
+var url = process.argv[2];
+request
+    .get(url)
+    .on('error', function (err) {
+        process.send({type: 2, error: err})
+    })
+    .on('response', function (response) {
+        // console.log(response.statusCode); // 200
+        if (response.statusCode == 200) {
+            process.send({type: 0, response: response})
+        } else {
+            process.send({type: 1, response: response})
 
-request({url: process.argv[2]})
-// request({url: 'http://localhost:7997/solr/vb_popbio/export?q=accession:*&fl=accession,collection_date_range_ss,phenotype_value_f,label_s&sort=accession+asc'})
+
+        }
+    })
     .pipe(stream)
     .pipe(jsonCsv.csv(options))
     .pipe(process.stdout);
+
+
+stream.on('end', function () {
+    // console.log('End of stream, exiting now');
+    // process.exit(0);
+    process.exitCode = 0;
+});
--- solr-json-to-csv.js
+++ solr-json-to-csv.js
@@ -1,23 +1,163 @@
-// 'use strict';
+'use strict';
 
-const process = require('child_process'),
+
+const spanwProcess = require('child_process'),
     fs = require('fs'),
-    http = require('http');
+    url = require('url'),
+    express = require('express'),
+    createError = require('http-errors');
+
+const app = express();
 
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, {
-            'Content-Type': 'text/csv',
-            'Content-Disposition': 'attachment; filename="export.csv";',
+/*
+ * Returns true if the request satisfies the following conditions:
+ *  - HTTP method (eg. GET,POST,..) is not in options.invalidHttpMethods
+ *  - Path (eg. /solr/update) is in options.validPaths
+ *  - All request query params (eg ?q=, ?stream.url=) not in options.invalidParams
+ */
+var validateRequest = function (request, options) {
+    var parsedUrl = url.parse(request.url, true),
+        path = parsedUrl.pathname,
+        queryParams = Object.keys(parsedUrl.query);
+
+    // console.log(parsedUrl);
+    // console.log(parsedUrl.pathname);
+    // console.log(options.validPaths.indexOf(parsedUrl.pathname));
+
+    return options.invalidHttpMethods.indexOf(request.method) === -1 &&
+        options.validPaths.indexOf(parsedUrl.pathname) !== -1 &&
+        queryParams.every(function (p) {
+            var paramPrefix = p.split('.')[0]; // invalidate not just "stream", but "stream.*"
+            return options.invalidParams.indexOf(paramPrefix) === -1;
+        });
+};
+
+
+// default options
+var defaultOptions = {
+    listenPort: 8000,
+    invalidHttpMethods: ['POST'],
+    validPaths: ['/solr/vb_popbio/smpExport', '/solr/vb_popbio/irExport'],
+    invalidParams: ['qt', 'stream'],
+    backend: {
+        host: 'localhost',
+        port: 7997
+    },
+    validator: validateRequest
+};
+
+var options = defaultOptions;
+
+
+/*
+ * Merge user-supplied options with @defaultOptions*.
+ */
+// var mergeDefaultOptions = function(defaultOptions, options) {
+//     var mergedOptions = {};
+//
+//     options = options || {};
+//     options.backend = options.backend || {};
+//     mergedOptions.invalidHttpMethods = options.invalidHttpMethods || defaultOptions.invalidHttpMethods;
+//     mergedOptions.validPaths = options.validPaths || defaultOptions.validPaths;
+//     mergedOptions.invalidParams = options.invalidParams || defaultOptions.invalidParams;
+//     mergedOptions.backend = options.backend || {};
+//     mergedOptions.backend.host = options.backend.host || defaultOptions.backend.host;
+//     mergedOptions.backend.port = options.backend.port || defaultOptions.backend.port;
+//     mergedOptions.validator = options.validator || defaultOptions.validator;
+//
+//     return mergedOptions;
+// };
+
+
+app.get('/solr/vb_popbio/*', function (req, res, next) {
+    // console.log(req.url);
+    var parsedUrl = url.parse(req.url, true),
+        path = parsedUrl.pathname;
+
+    var fUrl;
+
+    if (options.validator(req, options)) {
+
+        fUrl = 'http://' + options.backend.host + ':' + options.backend.port + req.url;
+        // console.log(JSON.stringify(parsedUrl));
+        var childProcess = spanwProcess.fork('child.js', [fUrl, path], {silent: true});
+
+    } else {
+        return next(createError(403, 'Invalid SOLR request'));
+    }
+
+
+    // Events
+    childProcess.on('message', function (data) {
+        if (data.type == 0) {
+            res.writeHead(200, {
+                'Content-Type': 'text/csv',
+                'Content-Disposition': 'attachment; filename="export.csv";',
+            });
+            this.stdout.pipe(res);
+
+        } else if (data.type == 1) {
+
+            childProcess.shutdown();
+            return next(createError(400, 'Bad request', {SolrResponse: data.response}));
+
+        } else {
+            childProcess.shutdown();
+            return next(createError(500, 'Internal Server Error', {expose: false}));
+
         }
-    );
+    });
 
-    var url = 'http://localhost:7997/solr/vb_popbio/export?q=accession:*&fl=accession,collection_date_range_ss,phenotype_value_f,label_s&sort=accession+asc';
-    var childProcess = process.spawn('node', ['child.js', url]);
-    // var childProcess = process.fork('child.js', [url]);
-    childProcess.stdout.pipe(res);
+// Helper function added to the child process to manage shutdown.
+    childProcess.onUnexpectedExit = function (code, signal) {
+        console.log("Child process terminated with code: " + code + ' ' + signal);
+    };
 
-    // process.stdout.pipe(res);
+    childProcess.on("exit", childProcess.onUnexpectedExit);
+
+    // A helper function to shut down the child.
+    childProcess.shutdown = function () {
+        // Get rid of the exit listener since this is a planned exit.
+        this.removeListener("exit", this.onUnexpectedExit);
+        this.kill("SIGTERM");
+    };
+
+
+// SIGTERM AND SIGINT will trigger the exit event.
+    process.once("SIGTERM", function () {
+        process.exit(0);
+    });
+    process.once("SIGINT", function () {
+        process.exit(0);
+    });
+// And the exit event shuts down the child.
+    process.once("exit", function () {
+        childProcess.shutdown();
+    });
+
+// This is a somewhat ugly approach, but it has the advantage of working
+// in conjunction with most of what third parties might choose to do with
+// uncaughtException listeners, while preserving whatever the exception is.
+    process.once("uncaughtException", function (error) {
+        // If this was the last of the listeners, then shut down the child and rethrow.
+        // Our assumption here is that any other code listening for an uncaught
+        // exception is going to do the sensible thing and call process.exit().
+        if (process.listeners("uncaughtException").length === 0) {
+            childProcess.shutdown();
+            throw error;
+        }
+    });
+
 
 });
-server.listen(8000);
+
+// Error handler
+app.use(errorHandler);
+
+app.listen(options.listenPort);
+
+function errorHandler(err, req, res, next) {
+    res.status(err.statusCode);
+    res.send(err.message);
+}
